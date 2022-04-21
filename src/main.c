1e25d94e22906dd6bc3a2707f5e2a8940134d63d
Bas yaw Hoitzing <bhoitzin@f0r3s17.codam.nl>
2022-04-21 15:29:45 +0200

    basic image

--- src/main.c
+++ src/main.c
@@ -1,10 +1,42 @@
+#include "../mlx42/include/MLX42/MLX42.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <memory.h>
+#define WIDTH 256
+#define HEIGHT 256
 
-int main(void)
+mlx_image_t	*g_img;
+
+void	hook(void *param)
 {
+	mlx_t	*mlx;
 
-	printf("hoi\n");
+	mlx = param;
+	if (mlx_is_key_down(param, MLX_KEY_ESCAPE))
+		mlx_close_window(param);
+	if (mlx_is_key_down(param, MLX_KEY_UP) || mlx_is_key_down(param, MLX_KEY_W))
+		g_img->instances[0].y -= 5;
+	if (mlx_is_key_down(param, MLX_KEY_DOWN) || mlx_is_key_down(param, MLX_KEY_S))
+		g_img->instances[0].y += 5;
+	if (mlx_is_key_down(param, MLX_KEY_LEFT) || mlx_is_key_down(param, MLX_KEY_A))
+		g_img->instances[0].x -= 5;
+	if (mlx_is_key_down(param, MLX_KEY_RIGHT) || mlx_is_key_down(param, MLX_KEY_D))
+		g_img->instances[0].x += 5;
+}
 
+int32_t	main(void)
+{
+	mlx_t	*mlx;
 
-	return 0;
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!mlx)
+		exit(EXIT_FAILURE);
+	g_img = mlx_new_image(mlx, 128, 128);
+	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
+	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
