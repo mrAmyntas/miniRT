f417961a79a8f1c25f946d20a2576fc3a3e5f537
Bas yaw Hoitzing <bhoitzin@f0r3s17.codam.nl>
2022-04-21 16:15:29 +0200

    removedglobal

--- src/main.c
+++ src/main.c
@@ -3,37 +3,49 @@
 #define WIDTH 1080
 #define HEIGHT 720
 
-mlx_image_t	*g_img;
+
+typedef struct 	s_data {
+
+	mlx_image_t	*mlx_img;
+	mlx_t		*mlx;
+}				t_data;
 
 void	hook(void *param)
 {
-	mlx_t	*mlx;
-
-	mlx = param;
-	if (mlx_is_key_down(param, MLX_KEY_ESCAPE))
-		mlx_close_window(param);
-	if (mlx_is_key_down(param, MLX_KEY_UP) || mlx_is_key_down(param, MLX_KEY_W))
-		g_img->instances[0].y -= 5;
-	if (mlx_is_key_down(param, MLX_KEY_DOWN) || mlx_is_key_down(param, MLX_KEY_S))
-		g_img->instances[0].y += 5;
-	if (mlx_is_key_down(param, MLX_KEY_LEFT) || mlx_is_key_down(param, MLX_KEY_A))
-		g_img->instances[0].x -= 5;
-	if (mlx_is_key_down(param, MLX_KEY_RIGHT) || mlx_is_key_down(param, MLX_KEY_D))
-		g_img->instances[0].x += 5;
+	t_data *data = param;
+	
+	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(data->mlx);
+	if (mlx_is_key_down(data->mlx, MLX_KEY_UP) || mlx_is_key_down(data->mlx, MLX_KEY_W))
+		data->mlx_img->instances[0].y -= 5;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_DOWN) || mlx_is_key_down(data->mlx, MLX_KEY_S))
+		data->mlx_img->instances[0].y += 5;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_LEFT) || mlx_is_key_down(data->mlx, MLX_KEY_A))
+		data->mlx_img->instances[0].x -= 5;
+	if (mlx_is_key_down(data->mlx, MLX_KEY_RIGHT) || mlx_is_key_down(data->mlx, MLX_KEY_D))
+		data->mlx_img->instances[0].x += 5;
 }
 
 int32_t	main(void)
 {
-	mlx_t	*mlx;
 
-	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
-	if (!mlx)
+	t_data data;
+
+	data.mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!data.mlx)
 		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, 64, 64);
-	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
-	mlx_image_to_window(mlx, g_img, 0, 0);
-	mlx_loop_hook(mlx, &hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
+	data.mlx_img = mlx_new_image(data.mlx, 64, 64);
+	memset(data.mlx_img->pixels, 255, data.mlx_img->width * data.mlx_img->height * sizeof(int));
+	mlx_image_to_window(data.mlx, data.mlx_img, 0, 0);
+	mlx_loop_hook(data.mlx, &hook, &data);
+	mlx_loop(data.mlx);
+	mlx_terminate(data.mlx);
 	return (EXIT_SUCCESS);
 }
+
+//Plane:
+//pl 0.0,0.0,-10.0 0.0,1.0,0.0 0,0,225
+//identifier: pl
+//x,y,z coordinates: 0.0,0.0,-10.0
+//3d normalized orientation vector. In range [-1,1] for each x,y,z axis: 0.0,0.0,1.0
+//R,G,B colors in range [0-255]: 0, 0, 255
