861a3d9ca1a25b4c70a82edd242a88633adf4c62
Bas yaw Hoitzing <bhoitzin@f0r3s17.codam.nl>
2022-04-21 22:44:46 +0200

    fixxx

--- src/plane.c
+++ src/plane.c
@@ -2,14 +2,48 @@
 
 int	plane(t_data *data)
 {
+	int i = 0;
+	int j = 0;
+
 	data->mlx = mlx_init(data->width, data->height, "MLX42", true);
 	if (!data->mlx)
 		exit(EXIT_FAILURE);
-	data->mlx_img = mlx_new_image(data->mlx, 64, 64);
+	data->mlx_img = mlx_new_image(data->mlx, data->width, data->height);
 	memset(data->mlx_img->pixels, 255, data->mlx_img->width * data->mlx_img->height * sizeof(int));
-	mlx_image_to_window(data->mlx, data->mlx_img, data->width/2-25, data->height/2-25);
-	mlx_loop_hook(data->mlx, &hook, &data);
+
+	data->color = create_rgbt(0, 255, 255, 255);//0xFFFFFF;
+
+	memset(data->mlx_img->pixels, 255, data->mlx_img->width * data->mlx_img->height * sizeof(int));
+	while (i < data->width)
+	{
+		j = 0;
+		while (j < data->height)
+		{
+			mlx_put_pixel(data->mlx_img, i, j, data->color);
+			j++;
+		}
+		i++;
+	}
+	mlx_image_to_window(data->mlx, data->mlx_img, 0, 0);
+
+	data->mlx_img = mlx_new_image(data->mlx, 100, 100);
+	memset(data->mlx_img->pixels, 255, data->mlx_img->width * data->mlx_img->height * sizeof(int));
+	data->color = 0x0000FFFF;//create_rgbt(255, 185, 46, 255);//0x00ff00f;
+	i = 0;
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			mlx_put_pixel(data->mlx_img, i, j, data->color);
+			j++;
+		}
+		i++;
+	}
+	mlx_image_to_window(data->mlx, data->mlx_img, 10, 10);
+	mlx_loop_hook(data->mlx, &hook, data);
 	mlx_loop(data->mlx);
 	mlx_terminate(data->mlx);
+
 	return 0;
 }
