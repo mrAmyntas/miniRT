9dbff3dd3c971add853f6f8efec12bc36f90bd8c
Bas yaw Hoitzing <bhoitzin@f0r3s17.codam.nl>
2022-04-21 15:13:23 +0200

    een main en makefile

---
+++ inc/miniRT.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   miniRT.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bhoitzin <bhoitzin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/09/24 19:53:41 by bhoitzin      #+#    #+#                 */
+/*   Updated: 2022/04/21 14:49:48 by bhoitzin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+# include <fcntl.h>
+
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("hoi\n");
+
+
+	return 0;
+}
