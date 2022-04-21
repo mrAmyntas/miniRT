6233f6b9baca9baa8103f3658da06ca992dc50e3
Milan andrei daniel Groen <mgroen@f0r3s18.codam.nl>
2022-04-21 21:33:19 +0200

    acl

--- src/get_scene.c
+++ src/get_scene.c
@@ -1,5 +1,10 @@
 #include "../inc/miniRT.h"
 
+int	create_rgb(int r, int g, int b)
+{
+	return (r << 16 | g << 8 | b);
+}
+
 void	free_strstr(char **str)
 {
 	int	i;
@@ -18,15 +23,83 @@ void	free_strstr(char **str)
 	}
 }
 
+double  ft_itod(char *str)
+{
+    int     x;
+    int     y;
+    double  digit;
+    char    **nums;
+
+    nums = ft_split(str, '.');
+    x = ft_atoi(nums[0]);
+    if (!nums[1])
+        y = 0;
+    else
+        y = ft_atoi(nums[1]);
+    digit = y;
+    if (nums[1])
+        digit /= pow(10, ft_strlen(nums[1]));
+    digit += x;
+    return (digit);
+}
+
 int     read_a(t_scene *scene, char **line)
 {
-    int colour;
+    char    **colours;
 
     if (scene->state[0])
         return (1);
     scene->state[0] = 1;
-    colour = create_trgb(24, ft_atoi(line[2]), ft_atoi(line[3]), ft_atoi(line[4]));
-    printf("%i\n", colour);
+    scene->a_ratio = ft_itod(line[1]);
+    colours = ft_split(line[2], ',');
+    scene->a_rgb = create_rgb(ft_atoi(colours[0]), ft_atoi(colours[1]), ft_atoi(colours[2]));
+    free_strstr(colours);
+    printf("A: %f %X\n", scene->a_ratio, scene->a_rgb);
+    return (0);
+}
+
+int     read_c(t_scene *scene, char **line)
+{
+    char    **coords;
+
+    if (scene->state[1])
+        return (1);
+    coords = ft_split(line[1], ',');
+    scene->c_x[0] = ft_itod(coords[0]);
+    scene->c_y[0] = ft_itod(coords[1]);
+    scene->c_z[0] = ft_itod(coords[2]);
+    free_strstr(coords);
+    coords = ft_split(line[2], ',');
+    scene->c_x[1] = ft_itod(coords[0]);
+    scene->c_y[1] = ft_itod(coords[1]);
+    scene->c_z[1] = ft_itod(coords[2]);
+    free_strstr(coords);
+    scene->c_fov = ft_atoi(line[3]);
+    printf("C: %f, %f, %f   %f, %f, %f  %i\n", scene->c_x[0], scene->c_y[0], scene->c_z[0], scene->c_x[1], scene->c_y[1], scene->c_z[1], scene->c_fov);
+    return (0);
+}
+
+int     read_l(t_scene *scene, char **line)
+{
+    char    **coords;
+
+    if (scene->state[2])
+        return (1);
+    coords = ft_split(line[1], ',');
+    scene->l_x = ft_itod(coords[0]);
+    scene->l_y = ft_itod(coords[1]);
+    scene->l_z = ft_itod(coords[2]);
+    free_strstr(coords);
+    scene->l_bright = ft_itod(line[2]);
+    coords = ft_split(line[3], ',');
+    scene->l_rgb = create_rgb(ft_atoi(coords[0]), ft_atoi(coords[1]), ft_atoi(coords[2]));
+    printf("L %f %f %f  %f  %X\n", scene->l_x, scene->l_y, scene->l_z, scene->l_bright, scene->l_rgb);
+    return (0);
+}
+
+int     read_pl(t_scene *scene, char **line)
+{
+    printf("...\n");
     return (0);
 }
 
@@ -35,23 +108,25 @@ void    read_scene(t_scene *scene, char *name)
     char    *str;
     char    **splitted_str;
     int     fd;
+    int     ret;
 
+    ret = 0;
     scene->state[0] = 0;
     scene->state[1] = 0;
     scene->state[2] = 0;
-    fd = open(ft_strjoin("../scenes/", name), O_RDONLY);
+    fd = open(name, O_RDONLY);
     str = get_next_line(fd);
-    while (str)
+    while (str && !ret)
     {
         splitted_str = ft_split(str, ' ');
         if (!strncmp(splitted_str[0], "A", 2))
-            read_a(scene, splitted_str);
-        //else if (!strncmp(splitted_str[0], "C", 2))
-        //    read_c(scene, splitted_str);
-        //else if (!strncmp(splitted_str[0], "L", 2))
-        //    read_l(scene, splitted_str);
-        //else if (!strncmp(splitted_str[0], "pl", 3))
-        //    read_pl(scene, splitted_str);
+            ret = read_a(scene, splitted_str);
+        else if (!strncmp(splitted_str[0], "C", 2))
+            ret = read_c(scene, splitted_str);
+        else if (!strncmp(splitted_str[0], "L", 2))
+            read_l(scene, splitted_str);
+        else if (!strncmp(splitted_str[0], "pl", 3))
+            read_pl(scene, splitted_str);
         //else if (!strncmp(splitted_str[0], "sp", 3))
         //    read_sp(scene, splitted_str);
         //else if (!strncmp(splitted_str[0], "cy", 3))
@@ -60,5 +135,10 @@ void    read_scene(t_scene *scene, char *name)
         free(str);
         str = NULL;
         str = get_next_line(fd);
+    }
+    if (ret)
+    {
+        printf("some kind of error\n");
+        exit(ret);
     }
 }
