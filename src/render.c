6bc35133577df4cba4e2b02bc5306e482708ad34
Bas yaw Hoitzing <bhoitzin@f0r3s15.codam.nl>
2022-05-12 15:14:21 +0200

    YEY

--- src/render.c
+++ src/render.c
@@ -1,5 +1,6 @@
 #include "../inc/miniRT.h"
 
+//angle:33.173466    Phit:25.000000 -20.000000 15.000000      hsl:21.176471 1.000000 0.500000      t:46.593991      shadow:1
 
 double	get_sp_angle(t_scene *scene, int num[2], t_vect3d Phit)
 {
@@ -8,9 +9,7 @@ double	get_sp_angle(t_scene *scene, int num[2], t_vect3d Phit)
 	t_vect3d	N;
 
     N = normalize_vector(subtract_vectors(Phit, scene->sp[num[1]].C));
-	ray.dir = normalize_vector(subtract_vectors(Phit, scene->light->ori));
-    ray.eye = scene->light->ori;
-    ray.dir = multiply_vector(ray.dir, -1);
+	ray.dir = normalize_vector(subtract_vectors(scene->light->ori, Phit));
     angle = acos(dot_product(N, ray.dir)) / (M_PI / 180);
 	return (angle);
 }
@@ -46,11 +45,14 @@ int	get_color(t_scene *scene, int num[2], double t, t_vect3d Phit)
 	int		shadow;
 
 	angle = get_angle(scene, num, Phit);
-	shadow = 0;
+	shadow = 1;
 	if (num[0] == PLANE)
-		return (calculate_light(angle, Phit, scene->pl[num[1]].hsl, scene, t, shadow));
+		return (calculate_light(angle, Phit, scene->pl[num[1]].hsl, scene, t, 1));
 	else if (num[0] == SPHERE)
-		return (calculate_light(angle, Phit, scene->sp[num[1]].hsl, scene, t, shadow));
+	{
+		//printf("angle:%f    Phit:%f %f %f        t:%f      shadow:%d\n", angle, Phit.x, Phit.y, Phit.z, t, shadow);
+		return (calculate_light(angle, Phit, scene->sp[num[1]].hsl, scene, t, 1));
+	}
 	else if (num[0] == CYLINDER)
 		return (scene->cy[num[1]].rgb);
 	return (-1);
@@ -106,22 +108,22 @@ double	find_closest_object(t_scene *scene, t_ray *ray, int num[2])
 	t[3] = smallest(t);
 	if (comp_d(t[0], t[3]))
 	{
-		num[0] = 0;
+		num[0] = PLANE;
 		num[1] = numb[0];
 	}
 	else if (comp_d(t[1], t[3]))
 	{
-		num[0] = 1;
+		num[0] = CYLINDER;
 		num[1] = numb[1];
 	}
 	else if (comp_d(t[2], t[3]))
 	{
-		num[0] = 2;
+		num[0] = SPHERE;
 		num[1] = numb[2];
 	}
 	else
 		num[0] = -1;
-	return (t[0]);
+	return (t[3]);
 }
 
 
@@ -134,9 +136,11 @@ int	find_pixel_color(t_scene *scene, double t, int num[2], t_vect3d Phit)
 	//check if light -> obj hits the same obj
 	ray.eye = scene->light->ori;
 	ray.dir = normalize_vector(subtract_vectors(Phit, ray.eye));
-	t2 = find_closest_object(scene, &ray, num);
+	t2 = find_closest_object(scene, &ray, num2);
 	Phit2 = add_vectors(ray.eye, multiply_vector(ray.dir, t2));
-	if (num2[0] == num[0] && num2[1] == num2[1] && compare_vectors(Phit, Phit2)) //light hits same obj as the camera ray at the same point
+	if (num[0] == 2)
+		printf("num:%d %d     num2:%d %d     Phit:%f %f %f         Phit2:%f %f %f\n", num[0], num[1], num2[0], num2[1], Phit.x, Phit.y, Phit.z, Phit2.x, Phit2.y, Phit2.z);
+	if (t2 != -1 && num[0] == num2[0] && num[1] == num2[1] && compare_vectors(Phit, Phit2)) //light hits same obj as the camera ray at the same point
 	{
 		//cast ray from camera to light, if this hits obj, check if it was BEFORE light, if it isnt, the light is hitting the BACK of the obj (only really matters for plane)
 		if (num[0] == 0 && check_if_plane_between_light_and_cam(scene, num)) // true means plane blocks light of itself
@@ -145,7 +149,7 @@ int	find_pixel_color(t_scene *scene, double t, int num[2], t_vect3d Phit)
 		{
  			//calculate_light(double angle, t_vect3d Phit, t_vect3d hsl, t_scene *scene, double t, int shadow)
 			//use the type+type_num (num[0] num[1] and distance (t2) to determine angle and calc color)
-			return (get_color(scene, num, t2, Phit));
+			return (get_color(scene, num2, t2, Phit));
 		}
 	}
 	// not lit
@@ -167,7 +171,9 @@ void	charge_my_lasers(t_data *data, t_scene *scene, int x, int y)
 		Phit = add_vectors(ray.eye, multiply_vector(ray.dir, t));
 		color = find_pixel_color(scene, t, num, Phit);
 		if (color != -1)
+		{
 			mlx_put_pixel(data->mlx_img, (data->width - x), (data->height - y), color);
+		}
 	}
 	//mlx_put_pixel(data->mlx_img, (data->width - x), (data->height - y), scene->pl[0].rgb);
 }
